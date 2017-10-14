f57f4b8069c7eabd19730dcc50967864ac9c056f
spike91 <emlecap7@mail.ru>
2017-10-14 19:26:17 +0300

    changed files directory

--- src/model/Category.php
+++ src/model/Category.php
@@ -1,13 +1,47 @@
 <?php
+class Category {
+private $id;
+    private $category_rus;
+    private $category_eng;
+    private $category_est;
+    
+    function __construct($id, $category_rus, $category_eng, $category_est) {
+        $this->id = $id;
+        $this->category_rus = $category_rus;
+        $this->category_eng = $category_eng;
+        $this->category_est = $category_est;
+    }
+    function getId() {
+        return $this->id;
+    }
 
-class Category
-{
-    public $id;
-    public $name;
+    function getCategory_rus() {
+        return $this->category_rus;
+    }
 
-     public function __construct($id, $name)
-     {
-         $this->id=$id;
-         $this->name=$name;
-     }
+    function getCategory_eng() {
+        return $this->category_eng;
+    }
+
+    function getCategory_est() {
+        return $this->category_est;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setCategory_rus($category_rus) {
+        $this->category_rus = $category_rus;
+    }
+
+    function setCategory_eng($category_eng) {
+        $this->category_eng = $category_eng;
+    }
+
+    function setCategory_est($category_est) {
+        $this->category_est = $category_est;
+    }
+
 }
+?>
