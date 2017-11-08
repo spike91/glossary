fb0d3e82eec0b199b3f7e22e0350c9cf9750febd
spike91 <emlecap7@mail.ru>
2017-11-08 11:15:08 +0200

    classes

--- app/Category.php
+++ app/Category.php
@@ -6,11 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $table = 'category';
-    protected $primaryKey='category_id';
+    protected $table = 'tcategory';
+    protected $primaryKey='id_category';
 
-    public function films() 
+    public function descriptions() 
     {
-       return $this->belongsToMany(Film::class);
+       return $this->belongsToMany(Description::class);
     }
 }
--- app/Word.php
+++ app/Word.php
@@ -4,19 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Actor extends Model
+class Word extends Model
 {
-    protected $table = 'actor';
-	protected $primaryKey='actor_id';
+    protected $table = 'tword';
+	protected $primaryKey='id_word';
 	
 	//By default, Eloquent expects created_at and updated_at columns to exist on your tables.
 	//If you do not wish to have these columns automatically managed by Eloquent, 
 	//set the $timestamps property on your model to false
 	public $timestamps = false;
 	
-	public function films()
-	{
-		return $this->belongsToMany('App\Film');
-	}
 }
 
