84cfa6465551922c7f6e214d637bdc081aaef9fa
spike91 <emlecap7@mail.ru>
2017-11-08 10:45:28 +0200

    v1

--- settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-const DB_DATABASE = "db_Mironova";
-const DB_HOST = "dev.vk.edu.ee";
-const DB_PORT = "5432";
-const DB_USERNAME = "t154462";
-const DB_PASSWORD="t154462";
-const DB_CHARSET="utf8";
-
